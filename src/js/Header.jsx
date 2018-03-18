5cbfadd4e5fc1a6fb93b16ed215b1a22784883b3
Krzysiek <len.krzysztof@gmail.com>
2018-03-18 09:46:54 +0100

    Create login button

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -1,19 +1,17 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
-const Header = (props) => {
-	// console.log(props);
-	return (
-		<header className="header">
-			<NavLink to="/">
-				<img src={require("../assets/logo.png")} className="logo" alt="trillo logo"/>
-			</NavLink>
-			<form action="#" className="search">
-				<input type="text" className="search__input" placeholder="Search hotels"/>
-			</form>
-			{/*<NavLink to="/" className="header__back">Back</NavLink>*/}
-		</header>
-	)
-};
+const Header = (props) => (
+	<header className="header">
+		<NavLink to="/">
+			<img src={require("../assets/logo.png")} className="logo" alt="trillo logo"/>
+		</NavLink>
+		<form action="#" className="search">
+			<input type="text" className="search__input" placeholder="Search hotels"/>
+		</form>
+		<button className="btn btn--login">Login</button>
+		{/*<NavLink to="/" className="header__back">Back</NavLink>*/}
+	</header>
+);
 
 export default Header;
