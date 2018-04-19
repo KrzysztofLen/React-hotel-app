b9c96d6b5aa367b6f147b7c7bc0816fdd399440c
Krzysiek <len.krzysztof@gmail.com>
2018-04-19 20:19:14 +0200

    Refactor navigation & fixed toggle button

--- src/js/Navigation.jsx
+++ src/js/Navigation.jsx
@@ -1,29 +1,5 @@
 import React, {Component} from 'react';
-
-import {HotelLink} from './Navigation/HotelLink';
-import {AddHotel} from './Navigation/AddHotel';
-import {BuyHotel} from './Navigation/BuyHotel';
-import {Cart} from './Navigation/Cart';
-import {Link} from 'react-router-dom';
-
-const numbers = [
-	{
-		link: "Hotel's",
-		href: "/"
-	},
-	{
-		link: "Buy Hotel",
-		href: "/buy"
-	},
-	{
-		link: "Cart",
-		href: "/cart"
-	},
-	{
-		link: "Add Hotel",
-		href: "/add"
-	}
-];
+import {BrowserRouter, NavLink} from 'react-router-dom';
 
 class LinkHotel extends React.Component {
 	constructor(props) {
@@ -31,48 +7,63 @@ class LinkHotel extends React.Component {
 		this.onSetActiveIndex = this.onSetActiveIndex.bind(this);
 	}
 
-	onSetActiveIndex() {
+	onSetActiveIndex = () => {
 		this.props.onClick(this.props.index);
 	};
 
+	// render() {
+	// 	return <React.Fragment>
+	// 		<li className={this.props.activeIndex ? "side-nav__item side-nav__item--active" : "side-nav__item"} onClick={this.onSetActiveIndex}>
+	// 		     {/*onClick={this.onSetActiveIndex}>*/}
+	// 			{/*<Link to={`${this.props.href}`} className="side-nav__link">*/}
+	// 				{/*<span>{this.props.link}</span>*/}
+	// 			{/*</Link>*/}
+	// 			<a href="#" className="side-nav__link">
+	// 				<span>{this.props.link}</span>
+	// 			</a>
+	// 		</li>
+	// 	</React.Fragment>
+	// }
+
+	//TODO Navigation routing
 	render() {
-		console.log(this.props);
-		// debugger;
-		return <React.Fragment>
-			<li className={this.props.activeIndex ? "side-nav__item side-nav__item--active" : "side-nav__item"}
-			    onClick={this.onSetActiveIndex}>
-				<Link to={`${this.props.href}`} className="side-nav__link">
-					<span>{this.props.link}</span>
-				</Link>
-			</li>
-		</React.Fragment>
+		return <BrowserRouter>
+			<React.Fragment>
+				<li className={this.props.activeIndex ? "side-nav__item side-nav__item--active" : "side-nav__item"}
+				    onClick={this.onSetActiveIndex}>
+					<NavLink to={`${this.props.href}`} className="side-nav__link">
+						<span>{this.props.link}</span>
+					</NavLink>
+				</li>
+			</React.Fragment>
+		</BrowserRouter>
 	}
 }
 
-class Navigation extends Component {
+class Navigation extends React.Component {
 	constructor(props) {
 		super(props);
 		this.onSetActiveIndex = this.onSetActiveIndex.bind(this);
 
 		this.state = {
 			activeIndex: 0
-		}
+		};
 	}
 
-	onSetActiveIndex = (idx) => {
-		this.setState({activeIndex: idx});
-		// console.log(this.state.activeIndex);
+	onSetActiveIndex = (index) => {
+		this.setState({
+			activeIndex: index
+		});
 	};
 
 	render() {
 		return (
 			<nav className="sidebar">
 				<ul className="side-nav">
-					{numbers.map((child, idx) => {
-						return <LinkHotel key={idx} index={idx} {...child} onClick={this.onSetActiveIndex}
-						                  activeIndex={this.state.activeIndex === idx}/>
+					{this.props.navlinks.map((child, index) => {
+						return <LinkHotel key={index} index={index} onClick={this.onSetActiveIndex}
+						                  activeIndex={this.state.activeIndex === index} {...child}/>
 					})}
-
 				</ul>
 
 				<div className="legal">
@@ -82,5 +73,26 @@ class Navigation extends Component {
 		)
 	}
 }
+
+Navigation.defaultProps = {
+	navlinks: [
+		{
+			link: "Hotel's",
+			href: "/"
+		},
+		{
+			link: "Buy Hotel",
+			href: "/buy"
+		},
+		{
+			link: "Cart",
+			href: "/cart"
+		},
+		{
+			link: "Add Hotel",
+			href: "/add"
+		}
+	]
+};
 
 export default Navigation;
--- src/js/routers/AppRouter.js
+++ src/js/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
 import SingleQuestion from './../SingleQuestion';
 import App from "../../App";
@@ -8,6 +8,30 @@ import {Cart} from './../Cart/Cart';
 import {BuyHotel} from './../BuyHotel/BuyHotel';
 import {AddHotel} from './../AddHotel/AddHotel';
 import app_data from '../../app_data';
+
+//TODO Make private route with addHotel component
+const fakeAuth = {
+	isAuthenticated: false
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={props =>
+			fakeAuth.isAuthenticated ? (
+				<Component {...props} />
+			) : (
+				<Redirect
+					to={{
+						pathname: "/",
+						state: { from: props.location }
+					}}
+				/>
+			)
+		}
+	/>
+);
+
 
 const AppRouter = () => {
 	const appData = app_data;
@@ -21,7 +45,7 @@ const AppRouter = () => {
 				/>
 				<Route path="/cart" component={Cart} />
 				<Route path="/buy" component={BuyHotel} />
-				<Route path="/add" component={AddHotel} />
+				<PrivateRoute path="/add" component={AddHotel} />
 			</Switch>
 		</div>
 	</BrowserRouter>
