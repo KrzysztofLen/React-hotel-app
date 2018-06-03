87cd03d8f751fb113b4d3d68fdc0c5c21147b6ef
Krzysiek <len.krzysztof@gmail.com>
2018-06-03 08:56:06 +0200

    Add callback to server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(process.env.PORT || 9999);
