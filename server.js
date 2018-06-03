40de75cfb5e28a5ee98b77561011e7a0b8ddef39
Krzysiek <len.krzysztof@gmail.com>
2018-06-03 10:22:06 +0200

    Moved code from API

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+// //############ TEMP
+// // require('./API/app');
+// //############
+//
+// app.use(express.static('client/build'));
+//
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+//
+// app.listen(process.env.PORT || 9999);
 
-//############ TEMP
-require('./API/server');
-//############
+const app = require('./API/app');
+const http = require('http');
 
-app.use(express.static('client/build'));
+const port = process.env.PORT || 1234;
+const server = http.createServer(app);
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 9999);
+server.listen(port);
