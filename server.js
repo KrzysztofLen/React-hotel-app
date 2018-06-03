60c6fc28e0b027745e65f357168152bff3fb7296
Krzysiek <len.krzysztof@gmail.com>
2018-06-03 07:38:03 +0200

    Create build script for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/client/build'));
 app.listen(process.env.PORT || 9999);
