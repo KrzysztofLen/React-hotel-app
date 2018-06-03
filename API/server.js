aa4914c384976c9f9f0d8dbb5023074f0a6cf0da
Krzysiek <len.krzysztof@gmail.com>
2018-06-03 10:56:14 +0200

    Edit static.json

--- API/server.js
+++ API/server.js
@@ -1,7 +1,7 @@
-const app = require('./app');
-const http = require('http');
-
-const port = process.env.PORT || 1234;
-const server = http.createServer(app);
-
-server.listen(port);
+// const app = require('./app');
+// const http = require('http');
+//
+// const port = process.env.PORT || 1234;
+// const server = http.createServer(app);
+//
+// server.listen(port);
