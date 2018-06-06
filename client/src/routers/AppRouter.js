76d64dc5d299d3fb7ce3bd2ef277d47dcaa81017
Krzysiek <len.krzysztof@gmail.com>
2018-06-06 19:08:43 +0200

    Test snapshot

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -59,15 +59,15 @@ class AppRouter extends React.Component {
 	componentDidMount() {
 
 		// fetch('/hotels', {
-		// 		// 	method: 'GET'
-		// 		// }).then(response => response.json())
-		// 		// 	.then((data) => {
-		// 		// 		this.setState({data: data.hotels});
-		// 		// 	}).catch(err => {
-		// 		// 	if (err.status !== 200) {
-		// 		// 		console.error('[Fetch Error :-S]', err);
-		// 		// 	}
-		// 		// });
+		// 	method: 'GET'
+		// }).then(response => response.json())
+		// 	.then((data) => {
+		// 		this.setState({data: data.hotels});
+		// 	}).catch(err => {
+		// 	if (err.status !== 200) {
+		// 		console.error('[Fetch Error :-S]', err);
+		// 	}
+		// });
 
 	}
 
