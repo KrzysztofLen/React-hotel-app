caa13d4efe9c7bd802adfc50e727722f9f770a55
Krzysiek <len.krzysztof@gmail.com>
2018-06-10 08:46:06 +0200

    Add new tests

---
+++ client/src/test/components/HotelImage.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import HotelImage from '../../components/HotelImage/HotelImage';
+import app_data from '../fixtures/app_data';
+
+test('it should render HotelImage component correctly', () => {
+	const wrapper = shallow(<HotelImage  image={app_data[0].image}/>);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('it should call onClick', () => {
+	const onClick = jest.fn();
+	const wrapper = shallow(<HotelImage onClick={onClick} image={app_data[0].image}/>);
+	wrapper.find('div').simulate('click');
+	expect(onClick).toHaveBeenCalled();
+});
