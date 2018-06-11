6ab92ac21ea4798a867591fe58d660dfd1a7aa67
Krzysiek <len.krzysztof@gmail.com>
2018-06-11 19:24:38 +0200

    Toggle active on views switch buttons

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -2,13 +2,32 @@ import React, {Component} from 'react';
 import {List} from "../SVG/List";
 import {Full} from "../SVG/Full";
 
+
 class Filters extends Component {
-	render(){
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activeId: 1
+		};
+
+		this.setActive = this.setActive.bind(this);
+	}
+
+	setActive(id) {
+		this.setState({activeId: id})
+	}
+
+	render() {
 		return (
 			<div className="filters">
 				<div className="filters__views">
-					<div className="filters__list"><List width="20" height="20"/></div>
-					<div className="filters__full"><Full width="20" height="20"/></div>
+					<div
+						className={this.state.activeId === 1 ? "filters__filter filters__filter--active" : "filters__filter"}
+						onClick={() => this.setActive(1)}><Full width="20" height="20"/></div>
+					<div
+						className={this.state.activeId === 2 ? "filters__filter filters__filter--active" : "filters__filter"}
+						onClick={() => this.setActive(2)}><List width="20" height="20"/></div>
 				</div>
 			</div>
 		)
