23ac489c449ae9c41d3253dafdb93e848bc88441
Krzysiek <len.krzysztof@gmail.com>
2019-03-09 16:12:26 +0100

    Google+ auth fix (Turn off API)

--- server/services/passport.js
+++ server/services/passport.js
@@ -111,6 +111,7 @@ module.exports = (passport) => {
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
+			userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
 			proxy: true
 		},
 		async (accessToken, refreshToken, profile, done) => {
