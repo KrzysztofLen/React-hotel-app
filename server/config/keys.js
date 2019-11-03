446f2fc0a397a285d6c638004964496c64d7798b
Krzysiek <len.krzysztof@gmail.com>
2019-11-03 18:49:47 +0100

    Update keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
 	// we are in production - return the prod set of keys
-	module.exports = require('./prod');
+	module.exports = require("./prod");
 } else {
 	// we are in development - return the keys
-	module.exports = require('./dev');
+	module.exports = require("./dev");
 }
